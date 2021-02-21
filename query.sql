9ebba8f3dd6fe5a6db5998cc901ab16ebf59c4f0
khartmann5 <hartmann.kt@gmail.com>
2021-02-21 00:14:51 -0800

    saving work on query code

---
+++ query.sql
@@ -0,0 +1,43 @@
+-- Show tables
+SELECT * FROM departments;
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s
+ON (e.emp_no = s.emp_no);
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+SELECT emp_no, first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date >= '1985-12-31' AND hire_date < '1987-01-01';
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT m.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM dept_manager m
+INNER JOIN departments d ON m.dept_no = d.dept_no
+INNER JOIN employees e ON m.emp_no = e.emp_no;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp m ON m.emp_no = e.emp_no
+INNER JOIN departments d ON m.dept_no = d.dept_no;
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT 
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
