db3c108a9515e02a40e9615171a2884fd6332143
khartmann5 <hartmann.kt@gmail.com>
2021-02-20 23:12:20 -0800

    adding csv files and schema code

---
+++ schema.sql
@@ -0,0 +1,101 @@
+-- Create schemas
+
+-- Create tables
+CREATE TABLE IF NOT EXISTS departments
+(
+    dept_no VARCHAR(10) NOT NULL,
+    dept_name VARCHAR(30),
+    PRIMARY KEY(dept_no)
+);
+
+CREATE TABLE IF NOT EXISTS titles
+(
+    title_id VARCHAR(10) NOT NULL,
+    title VARCHAR(20) NOT NULL,
+    PRIMARY KEY(title_id)
+);
+
+CREATE TABLE IF NOT EXISTS employees
+(
+    emp_no INTEGER NOT NULL,
+    emp_title_id VARCHAR(10) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    sex VARCHAR(2) NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY(emp_no)
+);
+
+CREATE TABLE IF NOT EXISTS dept_emp
+(
+    emp_no INTEGER NOT NULL,
+    dept_no VARCHAR(10) NOT NULL    
+);
+
+CREATE TABLE IF NOT EXISTS dept_manager
+(
+    dept_no VARCHAR(10) NOT NULL,
+    emp_no INTEGER NOT NULL    
+);
+
+CREATE TABLE IF NOT EXISTS salaries
+(
+    emp_no INTEGER NOT NULL,
+    salary INTEGER NOT NULL    
+);
+
+
+-- Create FKs
+ALTER TABLE dept_emp
+    ADD    FOREIGN KEY (emp_no)
+    REFERENCES employees(emp_no)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE dept_emp
+    ADD    FOREIGN KEY (dept_no)
+    REFERENCES departments(dept_no)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE dept_manager
+    ADD    FOREIGN KEY (emp_no)
+    REFERENCES employees(emp_no)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE dept_manager
+    ADD    FOREIGN KEY (dept_no)
+    REFERENCES departments(dept_no)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE employees
+    ADD    FOREIGN KEY (emp_title_id)
+    REFERENCES titles(title_id)
+    MATCH SIMPLE
+;
+  
+ALTER TABLE salaries
+    ADD    FOREIGN KEY (emp_no)
+    REFERENCES employees(emp_no)
+    MATCH SIMPLE
+;
+
+
+-- Drop tables
+DROP TABLE departments CASCADE;
+DROP TABLE titles CASCADE;
+DROP TABLE employees CASCADE;
+DROP TABLE dept_emp CASCADE;
+DROP TABLE dept_manager CASCADE;
+DROP TABLE salaries CASCADE;
+
+-- Show tables
+SELECT * FROM departments;
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
